302efdb3878ba5dd71a9feefa7ef0d74538c95a1
Nenad Livaic <nenad.livaic@cz.pwc.com>
2022-08-24 12:41:28 +0200

    dev: Formatting.

--- source/src/SparkRoseDigital_Template.WorkerServices/FooService/FooCommandConsumer.cs
+++ source/src/SparkRoseDigital_Template.WorkerServices/FooService/FooCommandConsumer.cs
@@ -19,12 +19,12 @@ namespace SparkRoseDigital_Template.WorkerServices.FooService
             protected override void ConfigureConsumer(IReceiveEndpointConfigurator endpointConfigurator, IConsumerConfigurator<FooCommandConsumer> consumerConfigurator)
             {
                 // Configure message retry with millisecond intervals.
-                //endpointConfigurator.UseMessageRetry(r => r.Intervals(100, 200, 500, 800, 1000));
+                // endpointConfigurator.UseMessageRetry(r => r.Intervals(100, 200, 500, 800, 1000));
                 // Creates only a queue, without topic and subscription.
-                //endpointConfigurator.ConfigureConsumeTopology = false;
+                // endpointConfigurator.ConfigureConsumeTopology = false;
 
                 // Use the outbox to prevent duplicate events from being published.
-                //endpointConfigurator.UseInMemoryOutbox();
+                // endpointConfigurator.UseInMemoryOutbox();
             }
         }
     }

a9b11d30f6cc4ca3e8a73caacbb5bbcca22c5735
Nenad Livaic <nenad.livaic@cz.pwc.com>
2023-04-21 19:06:58 +0200

    Testcontainers integration. [+semver: minor]

---
+++ source/tests/SparkRoseDigital_Template.Api.Tests/FooTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Docker.DotNet.Models;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SparkRoseDigital_Template.Api.Tests.Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SparkRoseDigital_Template.Api.Tests
+{
+    public class FooTest : IClassFixture<MsSqlTests>
+    {
+        private readonly CustomWebApplicationFactory<Startup> _webApplicationFactory;
+        private readonly ITestOutputHelper _testOutput;
+
+        public FooTest(
+            ITestOutputHelper testOutput,
+            MsSqlTests fixture)
+        {
+            _webApplicationFactory = new CustomWebApplicationFactory<Startup>(fixture);
+            _webApplicationFactory.ClientOptions.BaseAddress = new Uri("http://localhost/api/");
+            _testOutput = testOutput;
+        }
+
+        [Fact]
+        public async Task FooTest1()
+        {
+            // Arrange
+            var client = _webApplicationFactory.CreateClient();
+            using var ctx = new MsSqlDbBuilder(_testOutput, _webApplicationFactory.ConnectionString)
+                .BuildContext();
+            var initialCount = ctx.Foos.Count();
+
+            // Act
+            var response = await client.PostAsJsonAsync("foos", new
+            {
+                Text = "My_Test_Title"
+            });
+            using var ctx1 = new MsSqlDbBuilder(_testOutput, _webApplicationFactory.ConnectionString)
+                .BuildContext();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            Assert.Equal(initialCount + 1, ctx1.Foos.Count());
+        }
+    }
+}
--- source/tests/SparkRoseDigital_Template.Api.Tests/Helpers/CustomWebApplicationFactory.cs
+++ source/tests/SparkRoseDigital_Template.Api.Tests/Helpers/CustomWebApplicationFactory.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Data.Common;
 using System.Linq;
 using MassTransit;
-using MassTransit.Testing;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
@@ -10,17 +10,20 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Moq;
 using SparkRoseDigital_Template.Data;
 
 namespace SparkRoseDigital_Template.Api.Tests.Helpers
 {
     public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
     {
-        public SqliteConnection KeepAliveConnection { get; }
+        private readonly string _connectionString;
 
-        public CustomWebApplicationFactory()
+        public SqliteConnection KeepAliveConnection { get; }
+        public string ConnectionString => _connectionString;
+
+        public CustomWebApplicationFactory(MsSqlTests fixture)
         {
+            _connectionString = fixture.ConnectionString;
             KeepAliveConnection = new SqliteConnection("DataSource=:memory:");
             KeepAliveConnection.Open();
         }
@@ -47,15 +50,11 @@ namespace SparkRoseDigital_Template.Api.Tests.Helpers
                 services
                     .AddAuthentication("Test")
                     .AddScheme<TestAuthenticationOptions, TestAuthenticationHandler>("Test", null);
-                var descriptor = services.SingleOrDefault(d =>
-                    d.ServiceType == typeof(DbContextOptions<SparkRoseDigital_TemplateDbContext>));
-                if (descriptor != null)
-                {
-                    services.Remove(descriptor);
-                }
+                services.Remove(services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<SparkRoseDigital_TemplateDbContext>)));
+                services.Remove(services.SingleOrDefault(d => d.ServiceType == typeof(DbConnection)));
                 services.AddDbContext<SparkRoseDigital_TemplateDbContext>(options =>
                 {
-                    options.UseSqlite(KeepAliveConnection);
+                    options.UseSqlServer(_connectionString);
                 });
                 services.AddMassTransitTestHarness();
                 var sp = services.BuildServiceProvider();

1c98ecc3e5f9672090369c77d550a2852387b117
Nenad Livaic <nenad@revelator.com>
2024-06-12 10:07:32 +0200

    Key Vault generated. Secrets generated and used by the release pipeline. readme.md updated to reflect latest changes. [+semver: minor]

--- source/deployment/provisionResources.ps1
+++ source/deployment/provisionResources.ps1
@@ -4,6 +4,7 @@ Write-Host "##[warning]------ Create Resources START ------"
 $jsonResultAll = az deployment group create --resource-group $resourceGroupName --template-file ./deployment/iac.bicep --parameters environment=$environment projectName=$projectName sql_admin_username=$sqlAdminUsername sql_admin_password=$sqlAdminPassword authAuthority=$authAuthority authAudience=$authAudience authValidIssuer=$authValidIssuer | ConvertFrom-Json
 $appServiceWebName = $jsonResultAll.properties.outputs.appServiceWebName.value
 $sqlServerName = $jsonResultAll.properties.outputs.sqlServerName.value
+
 Write-Host "##vso[task.setvariable variable=appServiceWebName;isoutput=true]$appServiceWebName"
 Write-Host "##vso[task.setvariable variable=sqlServerName;isoutput=true]$sqlServerName"
 $dbConnection = $jsonResultAll.properties.outputs.dbConnection.value
